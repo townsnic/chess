dd0bf75f82ef1dd65dec3e702dd97044346a7321
townsnic <townsend.nick.r@gmail.com>
2024-10-20 23:13:04 -0600

    Created and passed positive test for register.

--- server/src/test/java/service/ServiceTest.java
+++ server/src/test/java/service/ServiceTest.java
@@ -1,4 +1,28 @@
 package service;
 
+import dataaccess.*;
+import model.*;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
 public class ServiceTest {
+
+    static private UserDAO userDAO;
+    static private AuthDAO authDAO;
+    static private UserService userService;
+
+    @BeforeAll
+    public static void init() {
+        userDAO = new MemoryUserDAO();
+        authDAO = new MemoryAuthDAO();
+        userService = new UserService(userDAO, authDAO);
+    }
+
+    @Test
+    public void registerUser() throws Exception {
+        UserData newUser = new UserData("username", "password", "email@gmail.com");
+        AuthData registrationResult = userService.registerUser(newUser);
+        Assertions.assertEquals(newUser, userDAO.getUser(newUser.username()));
+    }
 }
