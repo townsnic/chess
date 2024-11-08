3c71bf91dca4ad7d59ceabcdc9057974e0241c36
townsnic <townsend.nick.r@gmail.com>
2024-11-07 22:36:17 -0700

    Created client and pre-login repl. Added some pre-login functionality.

---
+++ client/src/main/java/ChessClient.java
@@ -0,0 +1,149 @@
+import java.util.Arrays;
+
+import com.google.gson.Gson;
+
+public class ChessClient {
+    private String username = null;
+    private final ServerFacade server;
+    private final String serverUrl;
+    private State state = State.LOGGEDOUT;
+
+    public ChessClient(String serverUrl) {
+        server = new ServerFacade(serverUrl);
+        this.serverUrl = serverUrl;
+    }
+
+    public String eval(String input) {
+        try {
+            var tokens = input.toLowerCase().split(" ");
+            var cmd = tokens[0];
+            var params = Arrays.copyOfRange(tokens, 1, tokens.length);
+            return switch (cmd) {
+                case "register" -> register(params);
+                case "login" -> login(params);
+                case "quit" -> "quit";
+//                case "create" -> adoptPet(params);
+//                case "list" -> adoptAllPets();
+//                case "join" -> "quit";
+//                case "observe" -> "quit";
+//                case "logout" -> "quit";
+                default -> help();
+            };
+        } catch (Exception ex) {
+            return ex.getMessage();
+        }
+    }
+
+    public String register(String... params) throws Exception {
+        if (params.length == 3) {
+            state = State.LOGGEDIN;
+            username = params[0];
+            return String.format("You successfully registered as %s.", username);
+        }
+        throw new Exception("Expected: <USERNAME> <PASSWORD> <EMAIL>");
+    }
+
+    public String login(String... params) throws Exception {
+        if (params.length == 2) {
+            state = State.LOGGEDIN;
+            username = params[0];
+            return String.format("You successfully logged in as %s.", username);
+        }
+        throw new Exception("Expected: <USERNAME> <PASSWORD>");
+    }
+
+//    public String rescuePet(String... params) throws ResponseException {
+//        assertSignedIn();
+//        if (params.length >= 2) {
+//            var name = params[0];
+//            var type = PetType.valueOf(params[1].toUpperCase());
+//            var pet = new Pet(0, name, type);
+//            pet = server.addPet(pet);
+//            return String.format("You rescued %s. Assigned ID: %d", pet.name(), pet.id());
+//        }
+//        throw new ResponseException(400, "Expected: <name> <CAT|DOG|FROG>");
+//    }
+//
+//    public String listPets() throws ResponseException {
+//        assertSignedIn();
+//        var pets = server.listPets();
+//        var result = new StringBuilder();
+//        var gson = new Gson();
+//        for (var pet : pets) {
+//            result.append(gson.toJson(pet)).append('\n');
+//        }
+//        return result.toString();
+//    }
+
+//    public String adoptPet(String... params) throws ResponseException {
+//        assertSignedIn();
+//        if (params.length == 1) {
+//            try {
+//                var id = Integer.parseInt(params[0]);
+//                var pet = getPet(id);
+//                if (pet != null) {
+//                    server.deletePet(id);
+//                    return String.format("%s says %s", pet.name(), pet.sound());
+//                }
+//            } catch (NumberFormatException ignored) {
+//            }
+//        }
+//        throw new ResponseException(400, "Expected: <pet id>");
+//    }
+
+//    public String adoptAllPets() throws ResponseException {
+//        assertSignedIn();
+//        var buffer = new StringBuilder();
+//        for (var pet : server.listPets()) {
+//            buffer.append(String.format("%s says %s%n", pet.name(), pet.sound()));
+//        }
+//
+//        server.deleteAllPets();
+//        return buffer.toString();
+//    }
+
+//    public String signOut() throws ResponseException {
+//        assertSignedIn();
+//        ws.leavePetShop(visitorName);
+//        ws = null;
+//        state = State.SIGNEDOUT;
+//        return String.format("%s left the shop", visitorName);
+//    }
+
+//    private Pet getPet(int id) throws ResponseException {
+//        for (var pet : server.listPets()) {
+//            if (pet.id() == id) {
+//                return pet;
+//            }
+//        }
+//        return null;
+//    }
+
+    public String help() {
+        if (state == State.LOGGEDOUT) {
+            return """
+                    - register <USERNAME> <PASSWORD> <EMAIL>
+                    - login <USERNAME> <PASSWORD>
+                    - quit
+                    - help
+                    """;
+        } else if (state == State.LOGGEDIN) {
+            return """
+                    - create <NAME>
+                    - list
+                    - join <ID> <WHITE|BLACK>
+                    - observe <ID>
+                    - logout
+                    - quit
+                    - help
+                    """;
+        }
+        return null;
+    }
+
+//    private void assertSignedIn() throws ResponseException {
+//        if (state == State.SIGNEDOUT) {
+//            throw new ResponseException(400, "You must sign in");
+//        }
+//    }
+}
---
+++ client/src/main/java/State.java
@@ -0,0 +1,4 @@
+public enum State {
+    LOGGEDOUT,
+    LOGGEDIN
+}
